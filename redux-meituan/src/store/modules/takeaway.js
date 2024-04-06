a9c649eb3f89b40bda08dabbceeef078d1eb641d
zjzj1996 <guaguaguaxia@gmail.com>
2024-04-06 11:29:36 +0800

    1

---
+++ redux-meituan/src/store/modules/takeaway.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const foodsStore = createSlice({
+  name: "foods",
+  initialState: {
+    foodsList: [],
+    activeIndex: 0,
+    cartList: [],
+  },
+  reducers: {
+    setFoodsList(state, action) {
+      state.foodsList = action.payload;
+    },
+    changeActiveIndex(state, action) {
+      state.activeIndex = action.payload;
+    },
+    addCart(state, action) {
+      const item = state.cartList.find(item => item.id === action.payload.id);
+      if (item) {
+        item.count++;
+      } else {
+        state.cartList.push({ ...action.payload, count: 1 });
+      }
+    },
+  },
+});
+
+const { setFoodsList, changeActiveIndex, addCart } = foodsStore.actions;
+
+const fetchFoodsList = () => {
+  return async (dispatch) => {
+    const res = await axios.get("http://localhost:3004/takeaway");
+    dispatch(setFoodsList(res.data));
+  };
+};
+
+export { fetchFoodsList, changeActiveIndex, addCart };
+
+const reducer = foodsStore.reducer;
+
+export default reducer;
