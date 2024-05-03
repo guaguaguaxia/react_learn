df53c8c86fadb54a077fd088e239329e9cb169e5
zjzj1996 <guaguaguaxia@Gmail.com>
2024-05-03 20:51:52 +0800

    1

--- react-bill-test/src/pages/Month/index.js
+++ react-bill-test/src/pages/Month/index.js
@@ -1,7 +1,54 @@
 import { DatePicker, NavBar } from 'antd-mobile'
 import './index.scss'
+import { useEffect, useMemo, useState } from 'react'
+import classNames from 'classnames'
+import dayjs from 'dayjs'
+import { useSelector } from 'react-redux'
+import _ from 'lodash'
+
 
 const Month = () => {
+    const billList = useSelector(state => state.bill.billList)
+
+
+    const [dateVisible, setDateVisible] = useState(false)
+
+    const [currentDate, setCurrentDate] = useState(() => {
+        return dayjs(new Date()).format('YYYY-MM')
+    })
+
+    const [currentMonthList, setCurrentMonthList] = useState([])
+
+    const monthGroup = useMemo(() => {
+        return _.groupBy(billList, (item) => dayjs(item.date).format('YYYY-MM'))
+    }, [billList])
+
+
+    const monthResult = useMemo(() => {
+        const pay = currentMonthList.filter(item => item.type === 'pay').reduce((a, c) => a + c.money, 0)
+        const income = currentMonthList.filter(item => item.type === 'income').reduce((a, c) => a + c.money, 0)
+        return {
+            pay,
+            income,
+            total: income + pay
+        }
+    }, [currentMonthList])
+
+    useEffect(() => {
+        const nowDate = dayjs(new Date()).format('YYYY-MM')
+        if (monthGroup[nowDate]) {
+            setCurrentMonthList(monthGroup[nowDate])
+        }
+    }, [monthGroup])
+
+    const onConfirm = (date) => {
+        setDateVisible(false)
+        const formatDate = dayjs(date).format('YYYY-MM')
+        setCurrentDate(formatDate)
+        setCurrentMonthList(monthGroup[formatDate] || [])
+    }
+
+
     return (
         <div className="monthlyBill">
             <NavBar className="nav" backArrow={false}>
@@ -10,23 +57,23 @@ const Month = () => {
 
             <div className="content">
                 <div className="header">
-                    <div className="date">
+                    <div className="date" onClick={() => setDateVisible(true)}>
                         <span className="text">
-                            2023 | 3月账单
+                            {currentDate + ''}月账单
                         </span>
-                        <span className="arrow expand"></span>
+                        <span className={classNames('arrow', dateVisible && 'expand')}></span>
                     </div>
                     <div className="twoLineOverview">
                         <div className="item">
-                            <span className="money">{100}</span>
+                            <span className="money">{monthResult.pay.toFixed(2)}</span>
                             <span className="type">支出</span>
                         </div>
                         <div className="item">
-                            <span className="money">{200}</span>
+                            <span className="money">{monthResult.income.toFixed(2)}</span>
                             <span className="type">收入</span>
                         </div>
                         <div className="item">
-                            <span className="money">{200}</span>
+                            <span className="money">{monthResult.total.toFixed(2)}</span>
                             <span className="type">结余</span>
                         </div>
                     </div>
@@ -35,7 +82,10 @@ const Month = () => {
                     className="kaDate" 
                     title="记账日期" 
                     precision='month'
-                    visible={false}
+                    visible={dateVisible}
+                    onCancel={() => setDateVisible(false)}
+                    onConfirm={onConfirm}
+                    onClose={() => setDateVisible(false)}
                     max={new Date()} 
                     
                     />
